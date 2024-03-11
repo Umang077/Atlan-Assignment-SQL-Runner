26618731326dc8e32200e87eb6c569c0a0af845a
Umang077 <96878284+Umang077@users.noreply.github.com>
2024-03-11 21:21:08 +0530

    Update Editor1.jsx

--- src/Editor1.jsx
+++ src/Editor1.jsx
@@ -1,6 +1,6 @@
 import './Editor1.css'
-import MainComponent from './Components/MainComponent/MainComponent';
-import Navbar1 from './Components/Navbar1/Navbar1'
+import MainComponent from './components/MainComponent/MainComponent';
+import Navbar1 from './components/Navbar1/Navbar1'
 export default function Editor1(){
     return (
         <div className='editor1-body'>
